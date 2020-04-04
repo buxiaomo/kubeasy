36a2a8a38e0d8e5c94214ab2c332af3eecd9fea0
buxiaomo <95112082@qq.com>
2020-04-04 14:02:12 +0800

    no message

--- scripts/runtime.sh
+++ scripts/runtime.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-apt install make curl -y
-apt-add-repository ppa:ansible/ansible
-apt-get update
-apt-get install ansible -y
+command -v yum &> /dev/null && export PKG=yum
+command -v apt &> /dev/null && export PKG=apt
+
+if [ ${PKG} == "yum" ];then
+  yum install epel-release -y
+  yum install make curl ansible -y
+fi
+
+if [ ${PKG} == "apt" ];then
+  apt-add-repository ppa:ansible/ansible
+  apt-get update
+  apt-get install ansible -y
+fi
