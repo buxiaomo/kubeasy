ed000465baeeded68a1d38717440f5c7ee25ded5
buxiaomo <95112082@qq.com>
2020-04-06 02:05:39 +0800

    update install ansible

--- scripts/runtime.sh
+++ scripts/runtime.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 command -v yum &> /dev/null && export PKG=yum
 command -v apt &> /dev/null && export PKG=apt
-
-if [ ${PKG} == "yum" ];then
-  yum install epel-release -y
-  yum install make rsync curl ansible -y
-fi
-
-if [ ${PKG} == "apt" ];then
-  apt-add-repository ppa:ansible/ansible
-  apt-get update
-  apt-get install make rsync curl ansible -y
-fi
+${PKG} install python python-pip -y
+pip install --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple ansible>=2.9.6
