8fa5232b7b4ea718ba331ccfb7bc9f0c22d38a10
buxiaomo <95112082@qq.com>
2020-04-06 21:11:04 +0800

    update kubernetes setting, and fix sshpass not install on runtime script

--- scripts/runtime.sh
+++ scripts/runtime.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 command -v yum &> /dev/null && export PKG=yum
 command -v apt &> /dev/null && export PKG=apt
-${PKG} install python python-pip -y
+${PKG} install python python-pip sshpass -y
 pip install --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple ansible>=2.9.6
