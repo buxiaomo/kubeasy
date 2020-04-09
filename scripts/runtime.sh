ff78ffe8a787c78aa52b799ec78ae5d480d03e18
buxiaomo <95112082@qq.com>
2020-04-09 19:08:21 +0800

    no message

--- scripts/runtime.sh
+++ scripts/runtime.sh
@@ -2,5 +2,5 @@
 command -v yum &> /dev/null && export PKG=yum
 command -v apt &> /dev/null && export PKG=apt
 ${PKG} install python sshpass -y
-curl https://bootstrap.pypa.io/get-pip.py | python
+command -v pip &> /dev/null || curl https://bootstrap.pypa.io/get-pip.py | python
 pip install --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple ansible
