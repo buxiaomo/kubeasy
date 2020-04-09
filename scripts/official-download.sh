2a687085bcdb01b04594dfdfd0c22a86886ae2b0
buxiaomo <95112082@qq.com>
2020-04-09 19:16:07 +0800

    use python3

--- scripts/official-download.sh
+++ scripts/official-download.sh
@@ -26,11 +26,12 @@ printb "Prepare docker ${DOCKER_VERSION} release ..."
 mkdir -p binaries/docker/${DOCKER_VERSION}
 grep -q "^${DOCKER_VERSION}\$" binaries/docker/${DOCKER_VERSION}/.docker 2>/dev/null || {
   if [ ! -f src/docker-${DOCKER_VERSION}.tgz ];then
-    printb "local package not found..."
+    printb "Download from the Internet..."
     curl -f --connect-timeout 20 --retry 5 --location --insecure https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}.tgz -o binaries/docker/${DOCKER_VERSION}/docker-${DOCKER_VERSION}.tgz
     tar -zxf binaries/docker/${DOCKER_VERSION}/docker-${DOCKER_VERSION}.tgz --strip-components 1 -C binaries/docker/${DOCKER_VERSION}
     rm -rf binaries/docker/${DOCKER_VERSION}/docker-${DOCKER_VERSION}.tgz
   else
+    printb "Use local binary packages..."
     tar -zxf src/docker-${DOCKER_VERSION}.tgz --strip-components 1 -C binaries/docker/${DOCKER_VERSION}
   fi
   echo ${DOCKER_VERSION} > binaries/docker/${DOCKER_VERSION}/.docker
@@ -42,11 +43,12 @@ printb "Prepare flannel ${FLANNEL_VERSION} release ..."
 mkdir -p binaries/flannel/${FLANNEL_VERSION}
 grep -q "^${FLANNEL_VERSION}\$" binaries/flannel/${FLANNEL_VERSION}/.flannel 2>/dev/null || {
   if [ ! -f src/flannel-v${FLANNEL_VERSION}-linux-amd64.tar.gz ];then
-    printb "local package not found..."
+    printb "Download from the Internet..."
     curl -f --connect-timeout 20 --retry 5 --location --insecure https://github.com/coreos/flannel/releases/download/v${FLANNEL_VERSION}/flannel-v${FLANNEL_VERSION}-linux-amd64.tar.gz -o binaries/flannel/${FLANNEL_VERSION}/flannel-v${FLANNEL_VERSION}-linux-amd64.tar.gz
     tar -zxf binaries/flannel/${FLANNEL_VERSION}/flannel-v${FLANNEL_VERSION}-linux-amd64.tar.gz -C binaries/flannel/${FLANNEL_VERSION}/ flanneld mk-docker-opts.sh
     rm -rf binaries/flannel/${FLANNEL_VERSION}/flannel-v${FLANNEL_VERSION}-linux-amd64.tar.gz
   else
+    printb "Use local binary packages..."
     tar -zxf src/flannel-v${FLANNEL_VERSION}-linux-amd64.tar.gz --strip-components 1 -C binaries/flannel/${FLANNEL_VERSION}/ flanneld mk-docker-opts.sh
   fi
   echo ${FLANNEL_VERSION} > binaries/flannel/${FLANNEL_VERSION}/.flannel
@@ -58,11 +60,12 @@ printb "Prepare etcd ${ETCD_VERSION} release ..."
 mkdir -p binaries/etcd/${ETCD_VERSION}
 grep -q "^${ETCD_VERSION}\$" binaries/etcd/${ETCD_VERSION}/.etcd 2>/dev/null || {
   if [ ! -f src/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz ];then
-    printb "local package not found..."
+    printb "Download from the Internet..."
     curl -f --connect-timeout 20 --retry 5 --location --insecure https://github.com/coreos/etcd/releases/download/v${ETCD_VERSION}/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz -o binaries/etcd/${ETCD_VERSION}/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz
     tar -zxf binaries/etcd/${ETCD_VERSION}/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz --strip-components 1 -C binaries/etcd/${ETCD_VERSION}/ etcd-v${ETCD_VERSION}-linux-amd64/etcd etcd-v${ETCD_VERSION}-linux-amd64/etcdctl 
     rm -rf binaries/etcd/${ETCD_VERSION}/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz
   else
+    printb "Use local binary packages..."
     tar -zxf src/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz --strip-components 1 -C binaries/etcd/${ETCD_VERSION}/ etcd-v${ETCD_VERSION}-linux-amd64/etcd etcd-v${ETCD_VERSION}-linux-amd64/etcdctl 
   fi
   echo ${ETCD_VERSION} > binaries/etcd/${ETCD_VERSION}/.etcd
@@ -74,6 +77,7 @@ printb "Prepare kubernetes ${KUBE_VERSION} release ..."
 mkdir -p binaries/kubernetes/${KUBE_VERSION}
 grep -q "^${KUBE_VERSION}\$" binaries/kubernetes/${KUBE_VERSION}/.kubernetes 2>/dev/null || {
   if [ ! -f src/kubernetes-client-linux-amd64.v${KUBE_VERSION}.tar.gz ] || [ ! -f src/kubernetes-server-linux-amd64.v${KUBE_VERSION}.tar.gz ];then
+    printb "Download from the Internet..."
     curl -f --connect-timeout 20 --retry 5 --location --insecure "https://storage.googleapis.com/kubernetes-release/release/v${KUBE_VERSION}/bin/linux/amd64/kube-apiserver" -o binaries/kubernetes/${KUBE_VERSION}/kube-apiserver
     curl -f --connect-timeout 20 --retry 5 --location --insecure "https://storage.googleapis.com/kubernetes-release/release/v${KUBE_VERSION}/bin/linux/amd64/kube-controller-manager" -o binaries/kubernetes/${KUBE_VERSION}/kube-controller-manager
     curl -f --connect-timeout 20 --retry 5 --location --insecure "https://storage.googleapis.com/kubernetes-release/release/v${KUBE_VERSION}/bin/linux/amd64/kube-scheduler" -o binaries/kubernetes/${KUBE_VERSION}/kube-scheduler
@@ -81,6 +85,7 @@ grep -q "^${KUBE_VERSION}\$" binaries/kubernetes/${KUBE_VERSION}/.kubernetes 2>/
     curl -f --connect-timeout 20 --retry 5 --location --insecure "https://storage.googleapis.com/kubernetes-release/release/v${KUBE_VERSION}/bin/linux/amd64/kube-proxy" -o binaries/kubernetes/${KUBE_VERSION}/kube-proxy
     curl -f --connect-timeout 20 --retry 5 --location --insecure "https://storage.googleapis.com/kubernetes-release/release/v${KUBE_VERSION}/bin/linux/amd64/kubelet" -o binaries/kubernetes/${KUBE_VERSION}/kubelet
   else
+    printb "Use local binary packages..."
     tar -zxf src/kubernetes-client-linux-amd64.v${KUBE_VERSION}.tar.gz --strip-components 3 -C binaries/kubernetes/${KUBE_VERSION}/
     tar -zxf src/kubernetes-server-linux-amd64.v${KUBE_VERSION}.tar.gz --strip-components 3 -C binaries/kubernetes/${KUBE_VERSION}/
   fi
@@ -93,10 +98,12 @@ printb "Prepare cni-plugins ${CNI_VERSION} release ..."
 mkdir -p binaries/cni-plugins/${CNI_VERSION}
 grep -q "^${CNI_VERSION}\$" binaries/cni-plugins/${CNI_VERSION}/.cni 2>/dev/null || {
   if [ ! -f src/cni-plugins-linux-amd64-v${CNI_VERSION}.tgz ];then
+    printb "Download from the Internet..."
     curl -f --connect-timeout 20 --retry 5 --location --insecure "https://github.com/containernetworking/plugins/releases/download/v${CNI_VERSION}/cni-plugins-linux-amd64-v${CNI_VERSION}.tgz" -o binaries/cni-plugins/${CNI_VERSION}/cni-plugins-linux-amd64-v${CNI_VERSION}.tgz
     tar -zxf binaries/cni-plugins/${CNI_VERSION}/cni-plugins-linux-amd64-v${CNI_VERSION}.tgz --strip-components 1 -C binaries/cni-plugins/${CNI_VERSION}/
     rm -rf binaries/cni-plugins/${CNI_VERSION}/cni-plugins-linux-amd64-v${CNI_VERSION}.tgz
   else
+    printb "Use local binary packages..."
     tar -zxf src/cni-plugins-linux-amd64-v${CNI_VERSION}.tgz --strip-components 1 -C binaries/cni-plugins/${CNI_VERSION}/
   fi
   echo ${CNI_VERSION} > binaries/cni-plugins/${CNI_VERSION}/.cni
--- scripts/runtime.sh
+++ scripts/runtime.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 command -v yum &> /dev/null && export PKG=yum
 command -v apt &> /dev/null && export PKG=apt
-${PKG} install python sshpass -y
-command -v pip &> /dev/null || curl https://bootstrap.pypa.io/get-pip.py | python
-pip install --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple ansible
+${PKG} install python3 python3-pip sshpass -y
+# command -v pip3 &> /dev/null || curl https://bootstrap.pypa.io/get-pip.py | python
+pip3 install --user --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple ansible
