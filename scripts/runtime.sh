04df6fdc5e30d7802cd0aa0c3f98ccb7ee89d91e
buxiaomo <95112082@qq.com>
2020-04-14 15:30:19 +0800

    fix ansible bin

--- scripts/runtime.sh
+++ scripts/runtime.sh
@@ -3,4 +3,4 @@ command -v yum &> /dev/null && export PKG=yum
 command -v apt &> /dev/null && export PKG=apt
 ${PKG} install python3 python3-pip sshpass -y
 # command -v pip3 &> /dev/null || curl https://bootstrap.pypa.io/get-pip.py | python
-pip3 install --user --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple ansible
+pip3 install --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple ansible
