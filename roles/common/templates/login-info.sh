aff1b1fa23b761d1697ac1fed6078fee4455a23f
buxiaomo <95112082@qq.com>
2020-04-19 21:05:10 +0800

    update role

---
+++ roles/common/templates/login-info.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+# Basic info
+HOSTNAME=`uname -n`
+ROOT=`df -Ph | grep xvda1 | awk '{print $4}' | tr -d '\n'`
+
+# System load
+MEMORY1=`free -t -m | grep Total | awk '{print $3" MB";}'`
+MEMORY2=`free -t -m | grep "Mem" | awk '{print $2" MB";}'`
+LOAD1=`cat /proc/loadavg | awk {'print $1'}`
+LOAD5=`cat /proc/loadavg | awk {'print $2'}`
+LOAD15=`cat /proc/loadavg | awk {'print $3'}`
+
+echo "
+===============================================
+ - Hostname............: $HOSTNAME
+ - Disk Space..........: $ROOT remaining
+===============================================
+ - CPU usage...........: $LOAD1, $LOAD5, $LOAD15 (1, 5, 15 min)
+ - Memory used.........: $MEMORY1 / $MEMORY2
+ - Swap in use.........: `free -m | tail -n 1 | awk '{print $3}'` MB
+===============================================
+"
