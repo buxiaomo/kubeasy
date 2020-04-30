8251a64b95dc7382cbbfb6bb3f86d1edcb511587
buxiaomo <95112082@qq.com>
2020-04-30 09:28:38 +0800

    no message

--- scripts/runtime.sh
+++ scripts/runtime.sh
@@ -2,5 +2,5 @@
 command -v yum &> /dev/null && export PKG=yum
 command -v apt &> /dev/null && export PKG=apt
 ${PKG} install python3 python3-pip sshpass curl -y
-pip3 install --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple ansible
+pip3 install --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple "ansible==2.9.7"
 chmod +x /usr/local/bin/ansible*
