bfe9caa906fe202b4b55e095420d5f27d8673f7c
buxiaomo <95112082@qq.com>
2023-01-10 18:57:05 +0800

    metrics-server upgrade to v0.6.2

--- plugins/filter/extension.py
+++ plugins/filter/extension.py
@@ -22,7 +22,7 @@ def component_version(value):
             },
             "plugin": {
                 "coredns": "1.8.4",
-                "metrics": "0.6.1",
+                "metrics": "0.6.2",
                 "pause": "3.5",
                 "nvidia": "0.12.3"
             }
