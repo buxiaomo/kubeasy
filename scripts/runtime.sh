008d45e0f4a0e1f9ed75fead674f97d88927fa59
buxiaomo <95112082@qq.com>
2023-01-30 20:44:55 +0800

    add  Arch linux support

--- scripts/runtime.sh
+++ scripts/runtime.sh
@@ -14,6 +14,8 @@ elif command -v zypper >/dev/null 2>&1; then
     zypper install -y git python3 sshpass wget curl vim jq tar
 elif command -v apk >/dev/null 2>&1; then
     apk add --no-cache sshpass wget curl git vim tar
+elif command -v pacman >/dev/null 2>&1; then
+    pacman -Syu --noconfirm python3 python-pip sshpass wget curl git vim tar
 else
     echo -e "\033[31merror: unknow OS version.\033[0m"
     exit 1
