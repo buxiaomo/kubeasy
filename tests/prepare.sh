fd4da86026b3299208ab027261e2eaad04496d9b
buxiaomo <95112082@qq.com>
2023-02-02 18:33:05 +0800

    update development environment

--- tests/prepare.sh
+++ tests/prepare.sh
@@ -2,6 +2,7 @@
 set -x
 export KUBE_VERSION=$1
 export KUBE_NETWORK=$2
+export inventory=$3
 
 command -v cloud-init >/dev/null 2>&1 && sudo cloud-init status --wait
 
@@ -32,20 +33,35 @@ centos)
     sudo curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-vault-8.5.2111.repo
     ;;
   esac
+  if [ $(cat /proc/sys/kernel/hostname) == 'master01' ]; then
+    sudo yum install git make python3-pip python3 -y
+  fi
   ;;
 ubuntu)
   sudo sed -i "s@http://.*archive.ubuntu.com@http://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
   sudo sed -i "s@http://.*security.ubuntu.com@http://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
+  if [ $(cat /proc/sys/kernel/hostname) == 'master01' ]; then
+    sudo yum install git make python3-pip python3 -y
+  fi
   ;;
 debian)
   sudo sed -i "s@https://.*deb.debian.org@http://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
   sudo sed -i "s@https://.*security.debian.org@http://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
+  if [ $(cat /proc/sys/kernel/hostname) == 'master01' ]; then
+    sudo yum install git make python3-pip python3 -y
+  fi
   ;;
 almalinux)
   sudo sed -e 's|^mirrorlist=|#mirrorlist=|g' -e 's|^# baseurl=https://repo.almalinux.org|baseurl=http://mirrors.aliyun.com|g' -i.bak /etc/yum.repos.d/almalinux*.repo
+  if [ $(cat /proc/sys/kernel/hostname) == 'master01' ]; then
+    sudo yum install git make python3-pip python3 -y
+  fi
   ;;
 rocky)
   sudo sed -e 's|^mirrorlist=|#mirrorlist=|g' -e 's|^#baseurl=http://dl.rockylinux.org/$contentdir|baseurl=http://mirrors.ustc.edu.cn/rocky|g' -i.bak /etc/yum.repos.d/rocky-extras.repo /etc/yum.repos.d/rocky.repo
+  if [ $(cat /proc/sys/kernel/hostname) == 'master01' ]; then
+    sudo yum install git make python3-pip python3 -y
+  fi
   ;;
 opensuse-leap)
   sudo zypper mr -da
@@ -53,27 +69,39 @@ opensuse-leap)
   sudo zypper ar -cfg 'http://mirrors.tuna.tsinghua.edu.cn/opensuse/distribution/leap/$releasever/repo/non-oss/' tuna-non-oss
   sudo zypper ar -cfg 'http://mirrors.tuna.tsinghua.edu.cn/opensuse/update/leap/$releasever/oss/' tuna-update
   sudo zypper ar -cfg 'http://mirrors.tuna.tsinghua.edu.cn/opensuse/update/leap/$releasever/non-oss/' tuna-update-non-oss
+  if [ $(cat /proc/sys/kernel/hostname) == 'master01' ]; then
+    sudo zupper in git make python3-pip python3
+  fi
   ;;
 arch)
   echo 'Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/$repo/os/$arch' >/etc/pacman.d/mirrorlist
-  pacman -Scc --noconfirm
-  pacman -Syy --noconfirm
-  pacman -S --noconfirm python3
+  sudo pacman -Scc --noconfirm
+  sudo pacman -Syy --noconfirm
+  if [ $(cat /proc/sys/kernel/hostname) == 'master01' ]; then
+    sudo pacman -S --noconfirm git make python3-pip python3
+  fi
+  ;;
+amzn)
+  if [ $(cat /proc/sys/kernel/hostname) == 'master01' ]; then
+    sudo yum install git make python3-pip python3 -y
+  fi
+  ;;
+fedora)
+  if [ $(cat /proc/sys/kernel/hostname) == 'master01' ]; then
+    sudo yum install git make python3-pip python3 -y
+  fi
   ;;
 esac
 
 if [ $(cat /proc/sys/kernel/hostname) == 'master01' ]; then
   set -e
-  sudo apt-get clean
-  sudo apt-get update
-  sudo apt-get install make python3 python3-pip -y
   sudo git clone -b ${KUBE_VERSION} https://github.com/buxiaomo/kubeasy.git /usr/local/src/kubeasy
   pushd /usr/local/src/kubeasy >/dev/null 2>&1
-    sudo pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-    sudo make runtime
-    sudo pip3 install pyOpenSSL --upgrade
-    [ -f ./inventory/kubeasy-dev.ini ] || cp ./inventory/template/vagrant-compatibility.template ./inventory/kubeasy-dev.ini
-    sudo make prepare
-    sudo make deploy REGISTRY_URL=http://192.168.56.10:5000 KUBE_NETWORK=${KUBE_NETWORK}
+    pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+    make runtime
+    pip3 install pyOpenSSL --upgrade
+    [ -f ./inventory/kubeasy-dev.ini ] || cp ./inventory/template/${inventory}.template ./inventory/kubeasy-dev.ini
+    make prepare
+    make deploy REGISTRY_URL=http://192.168.56.10:5000 KUBE_NETWORK=${KUBE_NETWORK}
   popd >/dev/null 2>&1
 fi
