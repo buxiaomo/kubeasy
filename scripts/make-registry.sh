c22c0c638ff1e06273641a7e41db05d6adfe6591
buxiaomo <95112082@qq.com>
2023-12-12 09:21:44 +0800

    upgrade calico and canal to v3.25.2

--- scripts/make-registry.sh
+++ scripts/make-registry.sh
@@ -18,21 +18,21 @@ docker run -d --name registry \
 registry:2.8.1
 
 # calico
-docker pull calico/cni:v3.25.1
-docker tag calico/cni:v3.25.1 ${REGISTRY_URL}/calico/cni:v3.25.1
-docker push ${REGISTRY_URL}/calico/cni:v3.25.1
+docker pull calico/cni:v3.25.2
+docker tag calico/cni:v3.25.2 ${REGISTRY_URL}/calico/cni:v3.25.2
+docker push ${REGISTRY_URL}/calico/cni:v3.25.2
 
-docker pull calico/pod2daemon-flexvol:v3.25.1
-docker tag calico/pod2daemon-flexvol:v3.25.1 ${REGISTRY_URL}/calico/pod2daemon-flexvol:v3.25.1
-docker push ${REGISTRY_URL}/calico/pod2daemon-flexvol:v3.25.1
+docker pull calico/pod2daemon-flexvol:v3.25.2
+docker tag calico/pod2daemon-flexvol:v3.25.2 ${REGISTRY_URL}/calico/pod2daemon-flexvol:v3.25.2
+docker push ${REGISTRY_URL}/calico/pod2daemon-flexvol:v3.25.2
 
-docker pull calico/node:v3.25.1
-docker tag calico/node:v3.25.1 ${REGISTRY_URL}/calico/node:v3.25.1
-docker push ${REGISTRY_URL}/calico/node:v3.25.1
+docker pull calico/node:v3.25.2
+docker tag calico/node:v3.25.2 ${REGISTRY_URL}/calico/node:v3.25.2
+docker push ${REGISTRY_URL}/calico/node:v3.25.2
 
-docker pull calico/kube-controllers:v3.25.1
-docker tag calico/kube-controllers:v3.25.1 ${REGISTRY_URL}/calico/kube-controllers:v3.25.1
-docker push ${REGISTRY_URL}/calico/kube-controllers:v3.25.1
+docker pull calico/kube-controllers:v3.25.2
+docker tag calico/kube-controllers:v3.25.2 ${REGISTRY_URL}/calico/kube-controllers:v3.25.2
+docker push ${REGISTRY_URL}/calico/kube-controllers:v3.25.2
 
 
 # canal
