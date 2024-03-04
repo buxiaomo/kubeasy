0df3d2ce2d83d05e5a4c33190599ff0eef009dff
buxiaomo <95112082@qq.com>
2024-03-04 12:28:23 +0800

    change tools to skopeo

--- scripts/push-to-private-registry.sh
+++ scripts/push-to-private-registry.sh
@@ -32,10 +32,6 @@ while getopts "h:r:u:p:" opt_name; do
 done
 
 set -e
-if [ -n ${username} ] || [ -n ${password} ]; then
-  echo ${password} | docker login --username ${username} --password-stdin ${host} >/dev/null
-fi
-
 for r in $(curl -s 127.0.0.1:5000/v2/_catalog | jq -r .repositories[]); do
   for t in $(curl -s 127.0.0.1:5000/v2/${r}/tags/list | jq -r .tags[]); do
     if [ "$repo" == "" ]; then
@@ -43,17 +39,7 @@ for r in $(curl -s 127.0.0.1:5000/v2/_catalog | jq -r .repositories[]); do
     else
       export toHarbor=${host}/${repo}/${r}:${t}
     fi
-    echo "[message] pull 127.0.0.1:5000/${r}:${t}"
-    docker pull 127.0.0.1:5000/${r}:${t} >/dev/null
-
-    echo "[message] rename 127.0.0.1:5000/${r}:${t} -> ${toHarbor}"
-    docker tag 127.0.0.1:5000/${r}:${t} ${toHarbor} >/dev/null
-
-    echo "[message] push ${toHarbor}"
-    docker push ${toHarbor} >/dev/null
-
-    echo "[message] clean ${toHarbor}"
-    docker rmi --force 127.0.0.1:5000/${r}:${t} ${toHarbor} >/dev/null
-    echo ""
+    echo "[sync] 127.0.0.1:5000/${r}:${t}"
+    skopeo copy --src-tls-verify=false --dest-username=${username} --dest-password=${password} --dest-tls-verify=false docker://127.0.0.1:5000/${r}:${t} docker://${toHarbor}
   done
 done
