a1da608fb109c7a1ee690390b97a573ba4194843
buxiaomo <95112082@qq.com>
2024-11-01 20:54:44 +0800

    upgrade calico and canal to v3.29.0

--- scripts/make-registry.sh
+++ scripts/make-registry.sh
@@ -18,23 +18,23 @@ docker run -d --name registry \
 registry:2.8.1
 
 # calico
-docker pull docker.io/calico/cni:v3.28.2
-docker tag docker.io/calico/cni:v3.28.2 ${REGISTRY_URL}/calico/cni:v3.28.2
-docker push ${REGISTRY_URL}/calico/cni:v3.28.2
+docker pull docker.io/calico/cni:v3.29.0
+docker tag docker.io/calico/cni:v3.29.0 ${REGISTRY_URL}/calico/cni:v3.29.0
+docker push ${REGISTRY_URL}/calico/cni:v3.29.0
 
-docker pull docker.io/calico/node:v3.28.2
-docker tag docker.io/calico/node:v3.28.2 ${REGISTRY_URL}/calico/node:v3.28.2
-docker push ${REGISTRY_URL}/calico/node:v3.28.2
+docker pull docker.io/calico/node:v3.29.0
+docker tag docker.io/calico/node:v3.29.0 ${REGISTRY_URL}/calico/node:v3.29.0
+docker push ${REGISTRY_URL}/calico/node:v3.29.0
 
-docker pull docker.io/calico/kube-controllers:v3.28.2
-docker tag docker.io/calico/kube-controllers:v3.28.2 ${REGISTRY_URL}/calico/kube-controllers:v3.28.2
-docker push ${REGISTRY_URL}/calico/kube-controllers:v3.28.2
+docker pull docker.io/calico/kube-controllers:v3.29.0
+docker tag docker.io/calico/kube-controllers:v3.29.0 ${REGISTRY_URL}/calico/kube-controllers:v3.29.0
+docker push ${REGISTRY_URL}/calico/kube-controllers:v3.29.0
 
 
 # canal
-docker pull docker.io/flannel/flannel:v0.24.3
-docker tag docker.io/flannel/flannel:v0.24.3 ${REGISTRY_URL}/flannel/flannel:v0.24.3
-docker push ${REGISTRY_URL}/flannel/flannel:v0.24.3
+docker pull docker.io/flannel/flannel:v0.24.4
+docker tag docker.io/flannel/flannel:v0.24.4 ${REGISTRY_URL}/flannel/flannel:v0.24.4
+docker push ${REGISTRY_URL}/flannel/flannel:v0.24.4
 
 
 # coredns
