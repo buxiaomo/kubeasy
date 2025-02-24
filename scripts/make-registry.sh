05a760ad1881546f3bc572e86737017a18e741b7
buxiaomo <95112082@qq.com>
2025-02-24 13:41:39 +0800

    upgrade antrea v2.3.0

--- scripts/make-registry.sh
+++ scripts/make-registry.sh
@@ -98,10 +98,10 @@ docker push ${REGISTRY_URL}/nvidia/k8s-device-plugin:v0.17.0
 
 
 # antrea
-docker pull antrea/antrea-agent-ubuntu:v2.2.0
-docker tag antrea/antrea-agent-ubuntu:v2.2.0 ${REGISTRY_URL}/antrea/antrea-agent-ubuntu:v2.2.0
-docker push ${REGISTRY_URL}/antrea/antrea-agent-ubuntu:v2.2.0
+docker pull antrea/antrea-agent-ubuntu:v2.3.0
+docker tag antrea/antrea-agent-ubuntu:v2.3.0 ${REGISTRY_URL}/antrea/antrea-agent-ubuntu:v2.3.0
+docker push ${REGISTRY_URL}/antrea/antrea-agent-ubuntu:v2.3.0
 
-docker pull antrea/antrea-controller-ubuntu:v2.2.0
-docker tag antrea/antrea-controller-ubuntu:v2.2.0 ${REGISTRY_URL}/antrea/antrea-controller-ubuntu:v2.2.0
-docker push ${REGISTRY_URL}/antrea/antrea-controller-ubuntu:v2.2.0
+docker pull antrea/antrea-controller-ubuntu:v2.3.0
+docker tag antrea/antrea-controller-ubuntu:v2.3.0 ${REGISTRY_URL}/antrea/antrea-controller-ubuntu:v2.3.0
+docker push ${REGISTRY_URL}/antrea/antrea-controller-ubuntu:v2.3.0
